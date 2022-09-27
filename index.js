f1d6893b734efbb86380683c5535cb7660f418b6
mohsenXAD <mohsenxad@gmail.com>
2022-09-27 18:14:47 +0330

    update authentication and authorization packages

--- index.js
+++ index.js
@@ -5,54 +5,117 @@ const auth = require('ir-hushyaar-middleware-panel-auth')(
 );
 
 const authorization = require('ir-hushyaar-middleware-panel-authorization')(
-    process.env.MONGODB_DATAAPI_APPID,
-    process.env.MONGODB_DATAAPI_APIKEY,
-    process.env.PROXY_URL,
-    process.env.REDIS_URL
+    {
+        MONGODB_DATAAPI_APPID:MONGODB_DATAAPI_APPID,
+        MONGODB_DATAAPI_APIKEY:MONGODB_DATAAPI_APIKEY,
+    },
+    PROXY_URL,
+    {
+        UPSTASH_REDIS_REST_TOKEN: UPSTASH_REDIS_REST_TOKEN,
+        UPSTASH_REDIS_REST_URL: UPSTASH_REDIS_REST_URL
+    }
+
 )
 
 const packageJson = require('./package.json');
 const userDeviceServices = require('./src');
 
-import { Router } from "cloudflare-router";
+import { Router } from 'itty-router'
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-    return res.text(`👌 ${packageJson.name}:${packageJson.version}`);
-});
-
-router.use("/userDevice/*",(req, res, next) => {
-    res.locals.middlewareUsed = true;
-    auth.chechAuth(req,res,next);
-});
-
-router.get('/userDevice/getAllByUser', async (req, res) => {
-    try {
-        const userId = req.user;
-        const userDeviceList = await userDeviceServices.getAllUserDeviceByUser(userId);
-        return res
-            .setHeader('Access-Control-Allow-Origin','*')
-            .statusCode(200)
-            .json({
-                deviceList: userDeviceList
-            });
-    } catch (error) {
-        console.log(error);
-        return res
-            .setHeader('Access-Control-Allow-Origin','*')
-            .statusCode(400)
-            .json({
-                error: error
-            });
+router.get("/", async (req, event) => 
+    {
+        return new Response(`👌 ${packageJson.name}:${packageJson.version}`)
     }
-})
+);
 
+
+router.get('/userDevice/getAllByUser',auth.chechAuth,async (req, event) => 
+    {
+            // console.log('here');
+            // const headers = event.request.headers;
+            // console.log(headers);
+            // console.log('deviceid')
+            // console.log(req.headers.get('deviceid'));
+            // req.headers.set("foo","bar");
+            // console.log('foo')
+            // console.log(req.headers.get('foo'));
+            // req.user = 'asdfsad';
+            console.log('user')
+            console.log(req.user);
+
+            try {
+                const userId = req.user;
+                const userDeviceList = await userDeviceServices.getAllUserDeviceByUser(userId);
+                const init = {
+                    headers: { 'content-type': 'application/json;charset=UTF-8', },
+                }
+                var result = {deviceList: userDeviceList};
+                return new Response(JSON.stringify(result, null, 2), init);
+            } catch (error) {
+                console.log(error);
+                return res
+                    .setHeader('Access-Control-Allow-Origin','*')
+                    .statusCode(400)
+                    .json({
+                        error: error
+                    });
+            }
+    }
+)
+
+
+router.get('/userDevice/getByUserAndDevice',auth.chechAuth, authorization.checkUserDeviceAccess ,async (req, event) => 
+    {
+        console.log('accessList')
+        console.log(req.accessList);
+        try {
+            const userId = req.user;
+            const deviceId = req.headers.get('deviceid');
+            const userDevice = await userDeviceServices.getAllUserDeviceByDeviceAndUser(deviceId,userId);
+            const init = {
+                headers: { 'content-type': 'application/json;charset=UTF-8', },
+            }
+            var result = {device: userDevice};
+            return new Response(JSON.stringify(result, null, 2), init);
+        } catch (error) {
+            console.log(error);
+            res.json(
+                {
+                    error: error
+                }
+            )
+        }
+    }
+)
+
+router.get('/userDevice/getByDevice',auth.chechAuth, authorization.checkUserDeviceAccess ,async (req, event) =>
+    {
+        try {
+            const deviceId = req.headers.get('deviceid');
+            const userDeviceList = await userDeviceServices.getAllUserDeviceByDevice(deviceId);
+            const init = {
+                headers: { 'content-type': 'application/json;charset=UTF-8', },
+            }
+            var result = {subscriberList: userDeviceList};
+            return new Response(JSON.stringify(result, null, 2), init);
+        } catch (error) {
+            console.log(error);
+            res.json(
+                {
+                    error: error
+                }
+            )
+        }
+    }
+)
 
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
     'Access-Control-Max-Age': '86400',
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, token, deviceid"
   };
   
 function handleOptions(request) {
@@ -88,30 +151,8 @@ function handleOptions(request) {
     }
   }
 
-// app.get('/userDevice/getByUserAndDevice', auth.chechAuth, authorization.checkUserDeviceAccess, async (req, res) =>
-// {
-//     try {
-//         const userId = req.user;
-//         const deviceId = req.headers.deviceid;
-//         const userDevice = await userDeviceServices.getAllUserDeviceByDeviceAndUser(deviceId,userId);
-//         res.json(
-//             {
-//                 device: userDevice
-//             }
-//         );
-//     } catch (error) {
-//         console.log(error);
-//         res.json(
-//             {
-//                 error: error
-//             }
-//         )
-//     }
-    
-// }
-// )
 
-// app.get('/userDevice/getByDevice', auth.chechAuth, authorization.checkUserDeviceAccess, async (req, res) =>
+
 // {
 //     try {
 //         const deviceId = req.headers.deviceid;
@@ -135,14 +176,37 @@ function handleOptions(request) {
 
 
 
-addEventListener("fetch", event => {
+// addEventListener("fetch", event => {
+//     if(event.request.method === 'OPTIONS'){
+//         event.respondWith(handleOptions(event.request));
+//     }else{
+//         return event.respondWith(
+//             router.serveRequest(event.request, {/* extra data */ })
+//                 .then(built => built.response)
+//         );
+//     }
+    
+// });
+
+// 404 for everything else
+router.all('*', () => new Response('Not Found.', { status: 404 }))
+
+// attach the router "handle" to the event handler
+addEventListener('fetch', event =>
+{
+    
     if(event.request.method === 'OPTIONS'){
         event.respondWith(handleOptions(event.request));
     }else{
-        return event.respondWith(
-            router.serveRequest(event.request, {/* extra data */ })
-                .then(built => built.response)
-        );
+        const request = event.request
+        const url = event.request.url;
+        const modifiedRequest = new Request(url, {
+            body: request.body,
+            headers: request.headers,
+            method: request.method,
+            redirect: request.redirect
+        })
+        event.respondWith(router.handle(modifiedRequest, event))
     }
-    
-});
+}
+)
